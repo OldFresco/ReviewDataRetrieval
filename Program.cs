26dec5402bb89e29e6144328cff0066eb9f5c32a
OldFresco <hinsonkojo@gmail.com>
2016-05-19 00:16:06 +0100

    add url & web client logic

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@
     {
         private static void Main(string[] args)
         {
+            var apiKey = "AIzaSyC198AyRLiVMrcVZV-Wfs8A3SimJNh8VLQ";
+            var playlistId = "PLP4CSgl7K7orSnEBkcBRqI5fDgKSs5c8o";
+            var baseUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50";
+            var needleDropPlaylistUrl = baseUrl+"&playlistId="+playlistId+"&key="+apiKey;
         }
     }
 }
