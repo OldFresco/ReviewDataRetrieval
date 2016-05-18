5f72d8a8e07deea9841c24d16bc4e2c673cdb896
Kojo Hinson <hinsonkojo@gmail.com>
2016-05-18 14:17:10 +0100

    Add more unit tests

---
+++ ReviewDataRetrievalTesting/ReviewRatingResolverTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using ReviewDataRetrieval;
+
+namespace ReviewDataRetrievalTesting
+{
+    public class ReviewRatingResolverTest
+    {
+        [Test]
+        public void GivenReviewDescriptionContainingRating_WhenRetrieveRatingFromDescriptionIsCalled_ThenValidRatingIsReturned()
+        {
+            const string descriptionString = @"who random0934j/49f test 305/1034 asg4b 1/10. sdf3/10 5/10";
+            const string expectedRating = "5/10";
+            var ratingResolver = new ReviewRatingResolver();
+            var actualRating = ratingResolver.RetrieveRatingFromDescription(descriptionString);
+            
+            Assert.AreEqual(expectedRating, actualRating);
+        }
+    }
+}
