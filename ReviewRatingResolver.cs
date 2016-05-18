b2176da59820c40ffd3bae8b5f4fdde089e651c8
Kojo Hinson <hinsonkojo@gmail.com>
2016-05-18 14:20:33 +0100

    Neaten up looping logic

--- ReviewRatingResolver.cs
+++ ReviewRatingResolver.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ReviewDataRetrieval
 {
     public class ReviewRatingResolver
     {
         private readonly List<string> ratingList = new List<string>();
-        
+
         public ReviewRatingResolver()
         {
             ratingList.Add("0/10");
@@ -20,25 +21,22 @@ namespace ReviewDataRetrieval
             ratingList.Add("9/10");
             ratingList.Add("10/10");
         }
-                
+
         public string RetrieveRatingFromDescription(string reviewDescription)
         {
             var descriptionContentArray = reviewDescription.Split(null);
             var rating = "rating not found";
-            
-            foreach(var word in descriptionContentArray)
+
+            foreach (var word in descriptionContentArray.Where(StringIsRating))
             {
-                if (StringIsRating(word))
-                {
-                    rating = word;
-                }
+                rating = word;
             }
-            return rating;        
+            return rating;
         }
-        
+
         public bool StringIsRating(string word)
         {
             return ratingList.Contains(word);
         }
-     }
+    }
 }
