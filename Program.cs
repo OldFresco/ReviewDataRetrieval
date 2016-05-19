6ba959a99a407787619b26676e87fd9ca0289760
OldFresco <hinsonkojo@gmail.com>
2016-05-19 02:26:33 +0100

    Add main process to entry point

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ReviewDataRetrieval
+﻿using System.Net.Http;
+
+namespace ReviewDataRetrieval
 {
     public class Program
     {
@@ -7,7 +9,29 @@
             var apiKey = "AIzaSyC198AyRLiVMrcVZV-Wfs8A3SimJNh8VLQ";
             var playlistId = "PLP4CSgl7K7orSnEBkcBRqI5fDgKSs5c8o";
             var baseUrl = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50";
-            var needleDropPlaylistUrl = baseUrl+"&playlistId="+playlistId+"&key="+apiKey;
+            var needleDropPlaylistUrl = baseUrl + "&playlistId=" + playlistId + "&key=" + apiKey;
+
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(needleDropPlaylistUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content;
+                    string responseString = responseContent.ReadAsStringAsync().Result;
+                    
+                    var parser = new ReviewDataParser(responseString);
+                    
+                    var listOfReviewDataRecords = parser.ParseJsonData();
+                    
+                    var csvDataSet = "";
+                    
+                    foreach (var record in listOfReviewDataRecords)
+                    {
+                        csvDataSet += record.ToCSV();
+                    }
+                }
+            }
         }
     }
 }
