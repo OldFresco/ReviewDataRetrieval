937416b1b8b12b90f04bc529f6b14567854f9d91
OldFresco <hinsonkojo@gmail.com>
2016-05-19 23:03:18 +0100

    Refactor mining execution logic into separate class

--- Program.cs
+++ Program.cs
@@ -7,52 +7,8 @@ namespace ReviewDataRetrieval
     {
         private static void Main(string[] args)
         {
-            if (args == null) throw new ArgumentNullException(nameof(args));
-            var apiKey = Properties.Settings.Default.apiKey;
-            var playlistId = Properties.Settings.Default.playlistId;
-            var baseUrl = Properties.Settings.Default.baseUrl;
-            var playlistUrl = baseUrl + "&playlistId=" + playlistId + "&key=" + apiKey;
-            var csvDataSet = "";
-            string nextPageToken;
-
-            //Get first page response to figure out whether we need to iterate over pages (as there's a 50 results per page limit)
-            using (var client = new HttpClient())
-            {
-                var response = client.GetAsync(playlistUrl).Result;
-                if (!response.IsSuccessStatusCode) return;
-
-                var responseContent = response.Content;
-                var responseString = responseContent.ReadAsStringAsync().Result;
-                var converter = new ReviewDataConverter(responseString);
-                var listOfReviewDataRecords = converter.ConvertJsonToReviewDataList();
-                csvDataSet += converter.ConvertReviewDataListToCsv(listOfReviewDataRecords);
-
-                var jsonExplorer = new YouTubeApiResponseExplorer(responseString);
-                nextPageToken = jsonExplorer.GetNextPageToken();
-            }
-
-            //Do iteration if needed.
-            while (nextPageToken != "no more tokens")
-            {
-                using (var client = new HttpClient())
-                {
-                    playlistUrl = baseUrl + "&pageToken=" + nextPageToken + "&playlistId=" + playlistId + "&key=" +
-                                  apiKey;
-                    var response = client.GetAsync(playlistUrl).Result;
-                    if (!response.IsSuccessStatusCode) return;
-
-                    var responseContent = response.Content;
-                    var responseString = responseContent.ReadAsStringAsync().Result;
-                    var converter = new ReviewDataConverter(responseString);
-                    var listOfReviewDataRecords = converter.ConvertJsonToReviewDataList();
-                    csvDataSet += converter.ConvertReviewDataListToCsv(listOfReviewDataRecords);
-
-                    var jsonExplorer = new YouTubeApiResponseExplorer(responseString);
-                    nextPageToken = jsonExplorer.GetNextPageToken();
-                }
-            }
-            Console.WriteLine(csvDataSet);
-            Console.ReadLine();
+            var dataMiner = new ReviewRatingDataMiner(Properties.Settings.Default.playlistId,"/filePath.csv");
+            dataMiner.Execut();
         }
     }
 }
---
+++ ReviewRatingDataMiner.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Net.Http;
+
+namespace ReviewDataRetrieval
+{
+    public class ReviewRatingDataMiner
+    {
+        private readonly string _outputFilePath;
+        private readonly string _ApiKey = Properties.Settings.Default.apiKey;
+        private readonly string _playlistId;
+        private readonly string _baseUrl = Properties.Settings.Default.baseUrl;
+        
+        public ReviewRatingDataMiner(string playlistId, string outputFilePath)
+        {
+            _playlistId = playlistId;
+            _outputFilePath = outputFilePath;
+        }              
+        
+        public void Execute()
+        {
+            var playlistUrl = baseUrl + "&playlistId=" + playlistId + "&key=" + apiKey;
+            var csvDataSet = "";
+            string nextPageToken;
+            
+            using (var client = new HttpClient())
+            {
+                do
+                { 
+                    var response = client.GetAsync(playlistUrl).Result;
+                    if (!response.IsSuccessStatusCode) return;
+                    var responseString = response.Content.ReadAsStringAsync().Result;
+                    var converter = new ReviewDataConverter(responseString);
+                    var listOfReviewDataRecords = converter.ConvertJsonToReviewDataList();
+                    csvDataSet += converter.ConvertReviewDataListToCsv(listOfReviewDataRecords);
+
+                    var jsonExplorer = new YouTubeApiResponseExplorer(responseString);
+                    nextPageToken = jsonExplorer.GetNextPageToken();
+                    playlistUrl  = (nextPageToken != null) ? = baseUrl + "&pageToken=" + nextPageToken + "&playlistId=" + playlistId + "&key=" +
+                                    apiKey; : "";
+                } while (nextPageToken != null);
+            }
+            
+            Console.WriteLine(csvDataSet);
+            Console.ReadLine();
+        }
+    }
+}
